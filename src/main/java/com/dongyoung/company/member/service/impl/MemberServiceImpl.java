297f37500d2e8f286657dd87832feebd80781c2f
dlehddud60 <dlehddud60@naver.com>
2024-04-26 16:19:05 +0900

    #6 MemberRead작업2

--- src/main/java/com/dongyoung/company/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/dongyoung/company/member/service/impl/MemberServiceImpl.java
@@ -2,12 +2,18 @@ package com.dongyoung.company.member.service.impl;
 
 import com.dongyoung.company.member.entity.Member;
 import com.dongyoung.company.member.model.FindRequestMemberInsertModel;
+import com.dongyoung.company.member.model.FindResponseMemberListModel;
+import com.dongyoung.company.member.model.FindResponseMemberModel;
 import com.dongyoung.company.member.repository.MemberRepository;
 import com.dongyoung.company.member.service.MemberService;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -25,5 +31,25 @@ public class MemberServiceImpl implements MemberService {
                 .build();
         em.persist(member);
         em.flush();
+    }
+
+    @Override
+    public List<FindResponseMemberListModel> list(Model model) {
+        List<Member> list = memberRepository.findAll();
+        List<FindResponseMemberListModel> listDTO = new ArrayList<>();
+        for (Member member : list) {
+            listDTO.add(new FindResponseMemberListModel(member.getMemberId(),member.getName(),member.getAddress()));
+        }
+        return listDTO;
+    }
+
+    @Override
+    public FindResponseMemberModel findByMemberId(Long memberId) {
+        Member member = em.find(Member.class, memberId);
+        FindResponseMemberModel memberModel = new FindResponseMemberModel(
+                member.getMemberId(),
+                member.getName(),
+                member.getAddress());
+        return memberModel;
     }
 }
