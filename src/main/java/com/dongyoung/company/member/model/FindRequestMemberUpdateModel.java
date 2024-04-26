88456af0f7f2bf79590945e53fc706e77bc1ba90
dlehddud60 <dlehddud60@naver.com>
2024-04-26 16:43:09 +0900

    #5 MemberRead update작업

--- src/main/java/com/dongyoung/company/member/model/FindRequestMemberUpdateModel.java
+++ src/main/java/com/dongyoung/company/member/model/FindRequestMemberUpdateModel.java
@@ -1,6 +1,7 @@
 package com.dongyoung.company.member.model;
 
 public record FindRequestMemberUpdateModel(
+        Long memberId,
         String name,
         String address
 ) {
--- src/main/java/com/dongyoung/company/member/service/MemberService.java
+++ src/main/java/com/dongyoung/company/member/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.dongyoung.company.member.service;
 
 import com.dongyoung.company.member.model.FindRequestMemberInsertModel;
+import com.dongyoung.company.member.model.FindRequestMemberUpdateModel;
 import com.dongyoung.company.member.model.FindResponseMemberListModel;
 import com.dongyoung.company.member.model.FindResponseMemberModel;
 import org.springframework.ui.Model;
@@ -13,4 +14,6 @@ public interface MemberService {
     List<FindResponseMemberListModel> list(Model model);
 
     FindResponseMemberModel findByMemberId(Long memberId);
+
+    void update(FindRequestMemberUpdateModel findRequestMemberUpdateModel);
 }
